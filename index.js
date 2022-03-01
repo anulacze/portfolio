b24a6ca6baa3daf9ea6b2bf128bb9a347d124eb5
Ania <Ania@Karolts-MacBook-Pro.local>
2022-03-01 17:23:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function pokazUkryj (element) {
+    if (element.style.display === 'inline') {
+        element.style.display = 'none';
+    } else {
+        element.style.display = 'inline';
+        }
+}
+
+const youTube = document.getElementById('youTube');
+const youTubeImg = document.getElementById('youTubeImg');
+youTube.addEventListener('click', function () {
+    pokazUkryj(youTubeImg);
+});
+
+const calculator = document.getElementById('calculator');
+const calculatorImg = document.getElementById('calculatorImg');
+calculator.addEventListener('click', function () {
+    pokazUkryj(calculatorImg);
+});
+
+const disney = document.getElementById('disney');
+const disneyImg = document.getElementById('disneyImg');
+disney.addEventListener('click', function () {
+    pokazUkryj(disneyImg);
+});
